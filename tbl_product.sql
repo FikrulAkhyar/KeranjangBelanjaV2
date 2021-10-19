5ae7ee0330c9e08bd2e2a532371cc7c4af7261a5
Fikrul Akhyar <fikrul18@mhs.unsyiah.ac.id>
2021-10-19 11:37:50 +0700

    Upload

---
+++ tbl_product.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 19, 2021 at 06:34 AM
+-- Server version: 10.4.18-MariaDB
+-- PHP Version: 7.3.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `tbl_product`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblproduct`
+--
+
+CREATE TABLE `tblproduct` (
+  `id` int(8) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `code` varchar(255) NOT NULL,
+  `image` text NOT NULL,
+  `price` varchar(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tblproduct`
+--
+
+INSERT INTO `tblproduct` (`id`, `name`, `code`, `image`, `price`) VALUES
+(1, 'FinePix Pro2 3D Camera', '3DcAM01', 'product-images/camera.jpg', '2.000.000'),
+(2, 'EXP Portable Hard Drive', 'USB02', 'product-images/external-hard-drive.jpg', '100.000'),
+(3, 'Luxury Ultra thin Wrist Watch', 'wristWear03', 'product-images/watch.jpg', '500.000'),
+(4, 'XP 1155 Intel Core Laptop', 'LPN45', 'product-images/laptop.jpg', '5.000.000');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tblproduct`
+--
+ALTER TABLE `tblproduct`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `product_code` (`code`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tblproduct`
+--
+ALTER TABLE `tblproduct`
+  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
